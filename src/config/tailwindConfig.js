affd48c850be36c5e1a321fe41209328d652d8be
JohnLloyd <johnlloydmayol@gmail.com>
2024-08-31 10:43:47 +0800

    entourage

--- src/config/tailwindConfig.js
+++ src/config/tailwindConfig.js
@@ -1,9 +1,19 @@
 tailwind.config = {
+  content: ["../*.{html,js}"],
   theme: {
     extend: {
       colors: {
         brown01: "#E8DBC7",
-        brown02: "#E1D0C2",
+        brown02: "#BB9F9B",
+        white01: "#FEFFFD",
+      },
+      screens: {
+        mobile: "378px",
+        "mobile-l": "427px",
+        tablet: "768px",
+      },
+      lineHeight: {
+        "leading-snug": "1.3",
       },
     },
   },
